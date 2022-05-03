b3cb8045438b256bdb280a86bd42a43f8d769ed2
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-03 22:08:18 +0100

    .env file

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const csvParser=require("csv-parse")
+const  {parse}=require("csv-parse")
+const fs=require("fs")
 
+const result=[]
+
+fs.createReadStream("./kepler_data.csv")
+  .on("data",(data)=>{
+     result.push(data)
+  })
+  .on("end",()=>{
+      console.log(result);
+      console.log("done reading our file");
+  })
+
+const parser=parse({
+
+})
