d44a26f4b41b55e3fc25a3053f5c456896e47d21
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-03 22:10:45 +0100

    .env file

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ fs.createReadStream("./kepler_data.csv")
   .on("data",(data)=>{
      result.push(data)
   })
+  .on("error",(error)=>{
+      console.log(error.message);
+  })
   .on("end",()=>{
       console.log(result);
       console.log("done reading our file");
